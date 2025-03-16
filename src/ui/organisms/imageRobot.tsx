6889743a5aea32a7e60c57ab59935e035944573a
nconsigny <nicolas.consigny@ethereum.org>
2025-03-16 07:38:50 +0100

    nerf robot image

--- src/ui/organisms/imageRobot.tsx
+++ src/ui/organisms/imageRobot.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function ImageRobot(): React.JSX.Element {
     return (
         <div className="absolute left-0 bottom-0 w-full px-4 pb-4">
-            <div className="relative w-[280px] mx-auto">
+            <div className="relative w-[256px] mx-auto">
                 <img 
                     src="/wallet.robbot-flipped.png" 
                     alt="WalletBeat Robot"
