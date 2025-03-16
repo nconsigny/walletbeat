d338ab5971b2965ab4692ca663645f967c436788
nconsigny <nicolas.consigny@ethereum.org>
2025-03-16 07:35:37 +0100

    alignement and robot picture

---
+++ src/ui/organisms/imageRobot.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export function ImageRobot(): React.JSX.Element {
+    return (
+        <div className="absolute left-0 bottom-0 w-full px-4 pb-4">
+            <div className="relative w-[280px] mx-auto">
+                <img 
+                    src="/wallet.robbot-flipped.png" 
+                    alt="WalletBeat Robot"
+                    className="w-full h-auto"
+                />
+            </div>
+        </div>
+    )
+} 
